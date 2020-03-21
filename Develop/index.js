58121010f12d1983b24f43972cea0b271a7fb7d6
imAnonAmi <mcintireaw@gmail.com>
2020-03-21 15:45:35 -0700

    Rough ideas in place. Needs a lot of work

--- Develop/index.js
+++ Develop/index.js
@@ -1,8 +1,26 @@
-const questions = [
+const inquirer = require("inquirer");
+const fs = require("fs");
+const util = require("util");
 
-];
+const writeToFile = util.promisify(fs.writeFile);
 
-function writeToFile(fileName, data) {
+function getUserName() {
+    return inquirer.prompt([
+      {
+        type: "input",
+        name: "gitname",
+        message: "What is your GitHub username?",
+        default: null,
+      },
+    ])
+
+  }
+
+//const questions = [
+//
+//];
+
+function writeToFile("README.md", md) {
 }
 
 function init() {
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,7 +1,31 @@
 function generateMarkdown(data) {
   return `
-# ${data.title}
-
+# ${data.title} <br>
+<br>
+## Description <br> 
+${data.description}
+<br>
+### Table of Contents <br>
+${data.toc}
+<br>
+#### Installation <br>
+${data.install}
+<br>
+#### Usage <br>
+${data.usage}
+<br>
+#### License <br>
+${data.license}
+<br>
+#### Contributors <br>
+${data.contributors}
+<br>
+### Tests! <br>
+${data.tests}
+<br>
+#### Questions <br>
+${data.questions}
+<br>
 `;
 }
 
